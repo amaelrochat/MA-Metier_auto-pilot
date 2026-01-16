0d7744c2e8d83943dad047ff1d55efc4c277d238
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-01-16 14:59:41 +0100

    feat: env variable

--- src/databases/database.py
+++ src/databases/database.py
@@ -8,9 +8,11 @@ Version : V 0.1
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
 #ENGINE CONFIGURATION
-engine = create_engine("sqlite:///src/databases/database.db")
+path = os.getenv("DATABASES_URI") or "sqlite:///src/databases/database.db"
+engine = create_engine(path)
 
 #DECLARATIVE BASE
 Base = declarative_base()
