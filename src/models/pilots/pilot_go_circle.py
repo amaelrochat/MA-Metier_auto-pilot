8467a6ddbd2758ce3660cd3237648270b10cf67f
amaeljampen <amael.jampen@eduvaud.ch>
2026-01-19 09:27:09 +0100

    feat: add airship go in circle class architecture

--- src/models/pilots/pilot_go_circle.py
+++ src/models/pilots/pilot_go_circle.py
@@ -1,2 +1,8 @@
 from src.models.pilots.pilot_interface import PilotInterface
 from SimConnect import AircraftRequests, SimConnect
+
+class shipGoInCircle(PilotInterface):
+    def __init__(self, airship: AircraftRequests):
+        self.airship = airship
+
+    def execute(self):
