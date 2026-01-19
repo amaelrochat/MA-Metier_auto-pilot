bd1c602c3e11637f35d420603db1a865ce654779
Amael Jampen <amaeljampen@eduvaud.ch>
2026-01-19 13:46:25 +0100

    fix: remove pilot_go_circle code

--- src/models/pilots/pilot_go_circle.py
+++ src/models/pilots/pilot_go_circle.py
@@ -1,8 +1,1 @@
-from src.models.pilots.pilot_interface import PilotInterface
 from SimConnect import AircraftRequests, SimConnect
-
-class shipGoInCircle(PilotInterface):
-    def __init__(self, airship: AircraftRequests):
-        self.airship = airship
-
-    def execute(self):
