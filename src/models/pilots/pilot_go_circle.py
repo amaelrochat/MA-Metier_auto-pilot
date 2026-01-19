796509d2b73deaea34aae1bb0522ebd81457229e
Amael Jampen <amaeljampen@eduvaud.ch>
2026-01-19 14:20:30 +0100

    feat: add base code for get API

--- src/models/pilots/pilot_go_circle.py
+++ src/models/pilots/pilot_go_circle.py
@@ -1,1 +1,16 @@
-from SimConnect import AircraftRequests, SimConnect
+import requests
+
+url = "https://api.exemple.com/data"
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+    print("Successful request")
+    data = response.json()
+    print(data)
+else:
+    print(f"Error : {response.status_code}")
+
+
+
