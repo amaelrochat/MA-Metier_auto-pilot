441fe1d23ea3faf126a66df5ae9e22aa91d69f92
amaeljampen <amael.jampen@eduvaud.ch>
2026-01-20 13:36:56 +0100

    feat: add values for the airship to fly in circle

--- src/models/pilots/pilot_go_circle.py
+++ src/models/pilots/pilot_go_circle.py
@@ -2,12 +2,16 @@ import requests
 
 url = "http://10.229.41.253/api/aircraft"
 
-response = requests.get(url)
+datas = {
+  "aileron_position": 0.3,
+  "elevator_position": 0.06,
+  "rudder_position": 0.0
+}
 
 
-if response.status_code == 200:
-    print("Successful request")
-    data = response.json()
-    print(data)
-else:
-    print(f"Error : {response.status_code}")
+while True:
+    response = requests.post(url, json=datas)
+
+    if response.status_code != 200:
+        print(f"Erreur : {response.status_code}")
+        break
