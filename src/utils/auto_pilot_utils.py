5324573efb22ac50c162b2df3c7d5ab872c95117
EthannSchneider <ethann.schneider@eduvaud.ch>
2026-01-29 09:06:05 +0100

    fix: enforce angle limits in maintain_angle and maintain_pitch_angle methods

--- src/utils/auto_pilot_utils.py
+++ src/utils/auto_pilot_utils.py
@@ -5,6 +5,7 @@ from src.services.aircraft_service import AircraftService
 class AutoPilotUtils:
     @staticmethod
     def maintain_angle(aircraft_service: AircraftService, angle_to_maintain: float):
+        angle_to_maintain = max(-0.8, min(0.8, angle_to_maintain))
         current_state = aircraft_service.get_aircraft_state()
         current_angle = current_state['plane_angle']
         angle_difference = -(angle_to_maintain - current_angle)
@@ -12,6 +13,7 @@ class AutoPilotUtils:
 
     @staticmethod
     def maintain_pitch_angle(aircraft_service: AircraftService, pitch_angle_to_maintain: float):
+        pitch_angle_to_maintain = max(-0.5, min(0.5, pitch_angle_to_maintain))
         current_state = aircraft_service.get_aircraft_state()
         current_pitch_angle = current_state['plane_pitch_angle']
         pitch_angle_difference = - \
@@ -40,7 +42,7 @@ class AutoPilotUtils:
         current_speed = plane_state['speed']
         speed_error = speed_to_maintain - current_speed
 
-        desired_plane_pitch_angle = max(-0.5, min(0.5, speed_error * 0.03))
+        desired_plane_pitch_angle = speed_error * 0.03
 
         AutoPilotUtils.maintain_pitch_angle(
             aircraft_service, desired_plane_pitch_angle)
